1a27f3ca77c32fb2402980ed0c429dd07f29933f
Martin Gross <martin@pc-coholic.de>
2013-07-21 10:21:39 +0200

    Adding ability-definitions for users and guests

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,9 +5,28 @@ class Ability
   	user ||= User.new # guest user
   	
   	if user.has_role? :admin
+      # Can do everything
   		can :manage, :all
+
+    elsif user.has_role? :user
+      # Can Login and Logout
+      can :create, :user_session
+      can :destroy, :user_session
+
+      # Can read and update Entries
+      can :read, Entry
+      can :update, Entry
+
+      # Can update own profile
+      can :create, User
+      can :update, User, :id => user.id
+      
   	else
-    	can :create, [:user_session, User]
+      # Can Login
+      can :create, :user_session
+
+      # Can create new Users and update profile
+      can :create, User
     end
   end
 end
