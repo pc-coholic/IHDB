7c3878667cf4c9fb8d6150de233806fddabc08e4
Martin Gross <martin@pc-coholic.de>
2013-10-21 17:26:39 +0200

    Adding initial/default admin-user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(:username => 'admin', :name => 'Default Admin user', :email => 'root@localhost.org', :roles => 'admin', :password => 'admin', :password_confirmation => 'admin')
